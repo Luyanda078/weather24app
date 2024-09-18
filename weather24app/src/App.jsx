82043e12c071c04e2a5273cf8a3413457cdb9b99
Luyanda <luyandashamase5@gmail.com>
2024-09-18 16:10:53 +0200

    Timelocation added

--- weather24app/src/App.jsx
+++ weather24app/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { TiWeatherPartlySunny } from "react-icons/ti";
 import TopButtons from './components/TopButtons';
 import Inputs from './components/inputs';
+import TimeandLocation from './components/TimeandLocation';
 
 function App() {
   return (
     <div className='mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700'>
      <TopButtons/>
      <Inputs/>
+     <TimeandLocation/>
     </div>
   )
 }
---
+++ weather24app/src/components/TimeandLocation.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function TimeandLocation() {
+  return (
+    <div>
+      <div className='flex item-center justify-center my-6'>
+
+      </div>
+      <p className='text-xl font-extralight '>
+      Wednesday, 18 September | Local time 16:07 AM
+      </p>
+
+
+      <div>
+        <p>
+          Berlin, DE
+        </p>
+      </div>
+    
+    </div>
+  )
+}
+
+export default TimeandLocation
