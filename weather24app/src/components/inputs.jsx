e64df1774d7bbf4ae23437975337f4e1f3caa0bf
Luyanda <luyandashamase5@gmail.com>
2024-09-18 11:58:12 +0200

    inputs and top buttons added and linked using map function(key&name)

---
+++ weather24app/src/components/inputs.jsx
@@ -0,0 +1,14 @@
+function Inputs() {
+  return (
+    <div className="flex flex-row justify-center my-6">
+        <div className="">
+            <input type="text" placeholder="search  by city..."
+            className="text-gray-500 text-xl font-light p-2 w-full shadow-xl"/>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Inputs
