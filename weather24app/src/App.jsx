bb29f23e395787bb25c39c3af9228f37aa0d28cd
Luyanda <luyandashamase5@gmail.com>
2025-03-25 11:32:48 +0200

    fix: Resolve weather data destructuring and formatting issues

--- weather24app/src/App.jsx
+++ weather24app/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
     const data = await getFormattedWeatherData({q: "berlin"});
     console.log(data);
   };
+
   getWeather();
 
 
--- weather24app/src/Resources/weatherResources.js
+++ weather24app/src/Resources/weatherResources.js
@@ -1,39 +1,38 @@
-import { format } from "morgan";
+import { DateTime } from 'luxon'; // Assuming you're using luxon for datetime formatting
 
-const API_KEY ="065d9e452efca2fb3259738f18538a45"
-const BASE_URL = "https://api.openweathermap.org/data/2.5/"
+const API_KEY = "065d9e452efca2fb3259738f18538a45";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/";
 
-const getWeatherData =(infoType, searchParams)=>{
+const getWeatherData = (infoType, searchParams) => {
     const url = new URL(BASE_URL + infoType);
     url.search = new URLSearchParams({...searchParams, appid: API_KEY});
-
-    return fetch(url).then((res)=>res.json());
+    return fetch(url).then((res) => res.json());
 };
 
-const iconUrlFromThere = (icon)=> `"http://openweathermap.org/img/wn/${icon}@2x.png"`
+const iconUrlFromThere = (icon) => `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
 const formatToLocalTime = (
     secs, 
     offset, 
-    format="cccc, dd LLL yyyy' | Local time: 'hh:mm a"
-)=> DateTime.fromSeconds(secs + offset, {zone: 'utc'}).toFromat(format)
+    format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a"
+) => DateTime.fromSeconds(secs + offset, {zone: 'utc'}).toFormat(format);
 
-
-const formatCurrent = (data)=>{
+const formatCurrent = (data) => {
     const {
-        coord:{lat,lan},
-        main:{temp, feels_like, temp_min, temp_max, humidity, pressure}, 
+        coord: {lat, lon}, // Fixed 'lan' to 'lon'
+        main: {temp, feels_like, temp_min, temp_max, humidity, pressure}, 
         name,
         dt,
-        sys:{country, sunrise, sunset},
-        wind:{speed},
-
+        sys: {country, sunrise, sunset},
+        wind: {speed},
+        weather, // Destructure weather from data
+        timezone
     } = data;
 
-    const {main: details, icon} = weather[0]
+    const {main: details, icon} = weather[0]; // Now weather is defined
     const formattedLocalTime = formatToLocalTime(dt, timezone);
 
-    return{
+    return {
         temp,
         feels_like,
         temp_min,
@@ -46,17 +45,19 @@ const formatCurrent = (data)=>{
         speed,
         details,
         icon: iconUrlFromThere(icon),
-        formattedLocalTime
-
+        formattedLocalTime,
+        lat,
+        lon
     };
 };
 
-const getFormattedWeatherData =async (searchParams) => {
+const getFormattedWeatherData = async (searchParams) => {
     const formattedCurrentWeather = await getWeatherData(
         "weather",
         searchParams
     ).then(formatCurrent);
     
-    return {  ...formattedCurrentWeather}
+    return { ...formattedCurrentWeather };
 };
-export default getFormattedWeatherData
+
+export default getFormattedWeatherData;
